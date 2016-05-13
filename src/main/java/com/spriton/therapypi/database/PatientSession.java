e42b079cff9908c4eafdcae59a181d948df2292d
Neal Sorensen <nealvs@gmail.com>
2016-05-12 21:35:50 -0600

    Repetitions require at least 5 degrees change.  Angle movement resets hold time.

--- src/main/java/com/spriton/therapypi/database/PatientSession.java
+++ src/main/java/com/spriton/therapypi/database/PatientSession.java
@@ -124,7 +124,7 @@ public class PatientSession {
             int firstAngle = readings.get(0).angle;
             int lastAngle = readings.get(readings.size() - 1).angle;
             boolean goingUp = firstAngle < lastAngle;
-            if (firstAngle != lastAngle) {
+            if (firstAngle != lastAngle && Math.abs(firstAngle - lastAngle) > Config.values.getInt("DIRECTION_CHANGE_MINIMUM", 5)) {
                 if(angleGoingUp != goingUp) {
                     // Direction has changed
                     angleGoingUp = goingUp;
@@ -132,6 +132,7 @@ public class PatientSession {
                         repetitions++;
                     }
                 }
+                holdStopwatch = Stopwatch.createStarted();
             }
         }
     }

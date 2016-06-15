ee89c2159cfd8a433420b20ae87cfd82fff78ad0
Neal Sorensen <nealvs@gmail.com>
2016-06-14 21:57:10 -0600

    Only switch motor pin state if needed to stop the flicker.

--- src/main/java/com/spriton/therapypi/components/hardware/MotorRelaySwitch.java
+++ src/main/java/com/spriton/therapypi/components/hardware/MotorRelaySwitch.java
@@ -1,26 +1,67 @@
 package com.spriton.therapypi.components.hardware;
 
 import com.pi4j.io.gpio.*;
+import com.spriton.therapypi.Config;
 import com.spriton.therapypi.components.Switch;
+import org.apache.log4j.Logger;
 
 public class MotorRelaySwitch extends Switch {
 
+    private static Logger log = Logger.getLogger(MotorRelaySwitch.class);
     private GpioController gpio;
     private GpioPinDigitalOutput switchPin;
 
     public MotorRelaySwitch(State state) {
         super(state);
         gpio = GpioFactory.getInstance();
-        switchPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Motor Relay Control", PinState.HIGH);
+        Pin pin = RaspiPin.GPIO_00;
+        switch(Config.values.getInt("MOTOR_SWITCH_PIN", 1)) {
+            case 0:
+                pin = RaspiPin.GPIO_00;
+                break;
+            case 1:
+                pin = RaspiPin.GPIO_01;
+                break;
+            case 2:
+                pin = RaspiPin.GPIO_02;
+                break;
+            case 3:
+                pin = RaspiPin.GPIO_03;
+                break;
+            case 4:
+                pin = RaspiPin.GPIO_04;
+                break;
+            case 5:
+                pin = RaspiPin.GPIO_05;
+                break;
+            case 6:
+                pin = RaspiPin.GPIO_06;
+                break;
+            case 7:
+                pin = RaspiPin.GPIO_07;
+                break;
+            case 8:
+                pin = RaspiPin.GPIO_08;
+                break;
+            case 9:
+                pin = RaspiPin.GPIO_09;
+                break;
+            case 10:
+                pin = RaspiPin.GPIO_10;
+                break;
+        }
+        switchPin = gpio.provisionDigitalOutputPin(pin, "Motor Relay Control", PinState.HIGH);
     }
 
     @Override
     public void applyState() throws Exception {
         if(switchPin != null) {
-            if (getState() == State.ON) {
+            if (getState() == State.ON && !switchPin.isHigh()) {
                 switchPin.high();
-            } else if (getState() == State.OFF) {
+                log.debug("Motor Switch: ON");
+            } else if (getState() == State.OFF && !switchPin.isLow()) {
                 switchPin.low();
+                log.debug("Motor Switch: OFF");
             }
         }
     }

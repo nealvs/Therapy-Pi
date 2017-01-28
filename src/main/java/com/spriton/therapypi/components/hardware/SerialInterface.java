8bb4956bca9e29600b47c771013a96acb46be31d
Neal Sorensen <nealvs@gmail.com>
2017-01-27 17:46:15 -0700

    Declared exception thrown

--- src/main/java/com/spriton/therapypi/components/hardware/SerialInterface.java
+++ src/main/java/com/spriton/therapypi/components/hardware/SerialInterface.java
@@ -3,16 +3,18 @@ package com.spriton.therapypi.components.hardware;
 import com.pi4j.io.serial.Serial;
 import com.pi4j.io.serial.SerialFactory;
 
+import java.io.IOException;
+
 public class SerialInterface {
 
     private static Serial serial;
 
-    public static void init() {
+    public static void init() throws IOException {
         serial = SerialFactory.createInstance();
         serial.open(Serial.DEFAULT_COM_PORT, 38400);
     }
 
-    public static void write(int value) {
+    public static void write(int value) throws IOException {
         serial.write((byte) value);
     }
 

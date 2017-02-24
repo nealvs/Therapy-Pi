d6ea640463aae259447fcc345e21b654f0f1d491
Neal Sorensen <nealvs@gmail.com>
2017-02-23 20:47:49 -0700

    Phidget library

--- installJar.sh
+++ installJar.sh
@@ -4,4 +4,6 @@ mvn install:install-file -Dfile=lib/commons-configuration2-2.0-beta2.jar \
     -DgroupId=org.apache.commons -DartifactId=commons-configuration2 \
     -Dversion=2.0-beta2 -Dpackaging=jar
 
-
+mvn install:install-file -Dfile=lib/phidget21.jar \
+    -DgroupId=com.phidgets -DartifactId=phidget \
+    -Dversion=2.1 -Dpackaging=jar

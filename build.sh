1e64b4ae078edf3767a8be135f0855c3359d390e
Neal Sorensen <nealvs@gmail.com>
2018-03-19 23:11:45 -0600

    Adding a cache-clear to the build.

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 mvn compile assembly:single
 cp target/therapy-pi-1.0-SNAPSHOT-jar-with-dependencies.jar therapypi.jar
 
+sudo rm -r  ~/.cache/Chromium/Default/Cache/f_*
+sudo rm -r  ~/.cache/Chromium/Default/Media\ Cache/f_*
